b175d5c7d1a722f9f5c6bbbbefc6a46eff70229f
kevinstradtman <stradtkt@outlook.com>
2023-07-27 15:32:35 -0400

    Initial Commit

---
+++ backend/ecommerce_java_angular/src/main/java/com/stradtkt/ecommercejavaangular/EcommerceJavaAngularApplication.java
@@ -0,0 +1,13 @@
+package com.stradtkt.ecommercejavaangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceJavaAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceJavaAngularApplication.class, args);
+    }
+
+}
---
+++ backend/ecommerce_java_angular/src/test/java/com/stradtkt/ecommercejavaangular/EcommerceJavaAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stradtkt.ecommercejavaangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceJavaAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
