4ea2fa0228c6cd1ca6001896462a6af363044b60
kevinstradtman <stradtkt@outlook.com>
2023-07-28 14:28:54 -0400

    Added frontend service to connect to the java backend

--- backend/ecommerce_java_angular/src/main/java/com/stradtkt/ecommercejavaangular/dao/ProductRepository.java
+++ backend/ecommerce_java_angular/src/main/java/com/stradtkt/ecommercejavaangular/dao/ProductRepository.java
@@ -2,5 +2,7 @@ package com.stradtkt.ecommercejavaangular.dao;
 
 import com.stradtkt.ecommercejavaangular.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {}
