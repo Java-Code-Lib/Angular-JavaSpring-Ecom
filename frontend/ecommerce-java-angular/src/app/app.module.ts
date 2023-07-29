e644d53041b63ec3a699cf528e79ebf80e9a901a
kevinstradtman <stradtkt@outlook.com>
2023-07-29 01:46:08 -0400

    Added frontend routes and product assets

--- frontend/ecommerce-java-angular/src/app/app.module.ts
+++ frontend/ecommerce-java-angular/src/app/app.module.ts
@@ -5,15 +5,26 @@ import {HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import {ProductService} from "./services/product.service";
+import {RouterModule, Routes} from "@angular/router";
+
+
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
