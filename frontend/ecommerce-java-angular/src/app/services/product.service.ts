bb144d1dba16665f2687fe00d743fceeee73c8ab
kevinstradtman <stradtkt@outlook.com>
2023-07-29 09:46:43 -0400

    Added frontend search capabilities and some more frontend changes to the code

--- frontend/ecommerce-java-angular/src/app/services/product.service.ts
+++ frontend/ecommerce-java-angular/src/app/services/product.service.ts
@@ -14,14 +14,23 @@ export class ProductService {
   //build category id on the backend based on what is selected on the frontend
   getProductList(theCategoryId: number): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
-    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
-      map(response => response._embedded.products)
-    );
+    return this.getProducts(searchUrl);
   }
 
   getProductCategories(): Observable<ProductCategory[]> {
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
       map(response => response._embedded.productCategory)
+    );
+  }
+
+  searchProducts(theKeyword: string): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+    return this.getProducts(searchUrl);
+  }
+
+  getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+      map(response => response._embedded.products)
     );
   }
 }
